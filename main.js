cf632ca396b64bef2b42d0877006921676d10acd
Shade <81486649+Srirang-Tumati@users.noreply.github.com>
2022-04-16 13:43:39 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
-function preload(){
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/2yt7fsZX/Mustache.png');
 }
 
-function setup(){
-    canvas = createCanvas(400,400);
+function setup() {
+    canvas = createCanvas(400, 400);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(400,400);
+    video.size(400, 400);
     video.hide();
 
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPose);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPose);
 }
 
-function draw(){
-    image(video,0,0,400,400);
+function draw() {
+    image(video, 0, 0, 400, 400);
+    image(mustache, noseX, noseY, 15, 15);
 }
 
-function take_snapshot(){
+function take_snapshot() {
     save("my_picture.png");
 }
 
-function modelLoaded(){
+function modelLoaded() {
     console.log("poseNet Is Initialized");
 }
 
-function gotPose(results){
-    if(results.length > 0){
+function gotPose(results) {
+    if (results.length > 0) {
         console.log(results);
-        console.log("nose x: "+results[0].pose.nose.x);
-        console.log("nose y: "+results[0].pose.nose.y);
-    };
+        noseX = results[0].pose.nose.x-5;
+        noseY = results[0].pose.nose.y-5;
+        console.log("noseX" + noseX);
+        console.log("noseY" + noseY);
+    }
 }
